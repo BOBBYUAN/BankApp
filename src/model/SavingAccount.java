52a925744f52c64db6b77b0d51b13f53c51f36e4
Bob <bobyuanwangcheng@gmail.com>
2019-10-25 12:58:27 -0700

    template set

--- src/model/SavingAccount.java
+++ src/model/SavingAccount.java
@@ -3,6 +3,9 @@ package model;
 import java.util.ArrayList;
 
 public class SavingAccount extends Account{
+
+    private double interestRate = 0.0;
+
     public SavingAccount(long accountId, ArrayList<Transaction> transactions, String branch, double balance) {
         super(accountId, transactions, branch, balance);
     }
