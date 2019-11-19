73b522647f789a40fa214f9c3176b66923bf0b1c
Bob <bobyuanwangcheng@gmail.com>
2019-11-18 22:12:13 -0800

    update readme

--- src/model/SavingAccount.java
+++ src/model/SavingAccount.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class SavingAccount extends Account{
 
     private double interestRate = 0.0;
-
     public SavingAccount(long accountId, ArrayList<Transaction> transactions, String branch, double balance) {
         super(accountId, transactions, branch, balance);
     }
