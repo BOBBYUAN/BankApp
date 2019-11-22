66ba5e6e28b3aee765ed655c958d613e39ef7406
Bob <bobyuanwangcheng@gmail.com>
2019-11-21 19:58:16 -0800

    Poject Structure Completed

--- cs174a/App.java
+++ cs174a/App.java
@@ -52,8 +52,8 @@ public class App implements Testable
 	{
 		// Some constants to connect to your DB.
 		final String DB_URL = "jdbc:oracle:thin:@cs174a.cs.ucsb.edu:1521/orcl";
-		final String DB_USER = "c##YoutNetID";
-		final String DB_PASSWORD = "YourPassword";
+		final String DB_USER = "c##wangcheng";
+		final String DB_PASSWORD = "7429699";
 
 		// Initialize your system.  Probably setting up the DB connection.
 		Properties info = new Properties();
